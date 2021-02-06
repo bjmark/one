4eb92198cd63218e3b60cacb7dce6beb4589ef99
mark <bjmark@sina.com>
2021-02-06 13:13:47 +0800

    hash_table

--- one_atom.c
+++ one_atom.c
@@ -8,11 +8,19 @@ struct element{
 static struct element *hash_table[1024];
 
 atom one_atom(char *name){
+    static struct element **hash_table = NULL;
+    if(hash_table == NULL){
+        hash_table = malloc(sizeof(element) * 1024);
+        for(int i = 0; i < 1024; i++)
+            hash_talbe[0] = NULL;
+    }
+
     int hash_code = 0;
     for(int i = 0; i < 16; i++){
-        hash_code += char[i];
-        if(char[i] == '\0') break;
+        hash_code += name[i];
+        if(name[i] == '\0') break;
     }
 
     hash_code %= 1024;
+
 }
