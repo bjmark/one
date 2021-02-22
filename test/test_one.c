39c86fd92c7b4496551ad32d385aa89ffe3f4937
mark <bjmark@sina.com>
2021-02-22 20:58:27 +0800

    finish

--- test/test_one.c
+++ test/test_one.c
@@ -1,18 +1,19 @@
 #include <assert.h>
 #include <stdio.h>
 #include "../source/one.h"
+#include "../source/one_assert.h"
 
 void test_one_new(void){
 	printf("test_one_new\n");
 
 	one *one_p = one_new(1);
-	assert(one_p != NULL);
-	assert(one_p->prev == NULL);
-	assert(one_p->next == NULL);
+	one_assert(one_p != NULL);
+	one_assert(one_p->prev == NULL);
+	one_assert(one_p->next == NULL);
 
 	one_p = one_new(3);
 
-	assert(one_p->prev == NULL);
+	one_assert(one_p->prev == NULL);
 	
 	int i = 1;
 	
@@ -21,7 +22,7 @@ void test_one_new(void){
 		i++;
 	}
 
-	assert(i == 3);
+	one_assert(i == 3);
 	
 	i = 1;
 	while(one_p->prev != NULL){
@@ -29,39 +30,39 @@ void test_one_new(void){
 		i++;
 	}
 
-	assert(i == 3);
+	one_assert(i == 3);
 }
 
 void test_one_last(void){
 	printf("test_one_last\n");
 
 	one one1 = {.next = NULL, .prev = NULL};
-	assert(one_last(&one1) == &one1);
+	one_assert(one_last(&one1) == &one1);
 
 	one one2 = {.next = NULL, .prev = &one1};
 	one1.next = &one2;
 	
-	assert(one_last(&one1) == &one2);
+	one_assert(one_last(&one1) == &one2);
 }
 
 void test_one_empty(){
 	printf("test_one_empty\n");
 	
 	one one1 = one_empty();
-	assert(one1.next == NULL);
-	assert(one1.prev == NULL);
+	one_assert(one1.next == NULL);
+	one_assert(one1.prev == NULL);
 }
 
 void test_one_parent(void){
 	printf("test_one_parent\n");
 
 	one one1 = one_empty();
-	assert(one_parent(&one1) == NULL);
+	one_assert(one_parent(&one1) == NULL);
 
 	one one2 = one_empty();
 	one1.next = &one2;
 
-	assert(one_parent(&one1) == &one2);
+	one_assert(one_parent(&one1) == &one2);
 }
 
 void test_one_find(void){
@@ -71,13 +72,13 @@ void test_one_find(void){
 	one1.name = one_atom("abc");
 	one1.type = one_atom("char_p");
 
-	assert(one_find(&one1, "abc", "char_p") != NULL);
+	one_assert(one_find(&one1, "abc", "char_p") != NULL);
 
 	one one2 = one_empty();
 	one2.name = one_atom("efg");
 	one1.next = &one2;
 
-	assert(one_find(&one1, "efg", "char_p") == NULL);
+	one_assert(one_find(&one1, "efg", "char_p") == NULL);
 }
 
 one one_fun(one *self, one arg){
@@ -95,18 +96,29 @@ void test_one_call(void){
 
 	one arg1 = {._int = 1};
 
-	assert(one_call(&one1, "fun", arg1)._int == 1);
+	one_assert(one_call(&one1, "fun", arg1)._int == 1);
 }
 
 int main(void){
-	printf("start testing...\n");
+	printf("start testing...\n\n");
 	
-	test_one_new();
-	test_one_last();
-	test_one_empty();
-	test_one_parent();
-	test_one_find();
-	test_one_call();
+	//test_one_new();
+	one_test_success(test_one_new);
+
+	//test_one_last();
+	one_test_success(test_one_last);
+
+	//test_one_empty();
+	one_test_success(test_one_empty);
+
+	//test_one_parent();
+	one_test_success(test_one_parent);
+
+	//test_one_find();
+	one_test_success(test_one_find);
+
+	//test_one_call();
+	one_test_success(test_one_call);
 
 	printf("test end\n");
 	return 1;
