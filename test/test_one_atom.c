73de974ceff3b2e0198eab3327ef1869025ee446
mark <bjmark@sina.com>
2021-02-23 22:12:16 +0800

    test_one_atom using one_assert

--- test/test_one_atom.c
+++ test/test_one_atom.c
@@ -1,16 +1,28 @@
 #include <assert.h>
 #include <stdio.h>
 #include "../source/one.h"
+#include "../source/one_assert.h"
 
-int main(void){
+void test_one_atom(void){
+	printf("test_one_atom\n");
+	
 	atom a = one_atom("abc");
 	atom b = one_atom("abc");
-	assert(a == b);
+	one_assert(a == b);
 
 	atom c = one_atom("123");
-	assert(a != c);
+	one_assert(a != c);
 
 	atom d = one_atom("123");
-	assert(c = d);
+	one_assert(c = d);
+}
+
+int main(void){
+	printf("start testing...\n\n");
+	
+	one_test_success(test_one_atom);
+
+	printf("test end\n");
+	return 1;
 }
 
