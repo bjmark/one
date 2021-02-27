719def71136ea4698c9405928c9ab25c9bf0c105
mark <bjmark@sina.com>
2021-02-27 13:04:31 +0800

    string.str, string.respace

--- source/one_string.c
+++ source/one_string.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
+
 #include "one.h"
+#include "one_assert.h"
 
 static one one_string_new(one *self, one arg){
 	one *p = one_new(1);
@@ -24,11 +26,30 @@ static one one_string_new(one *self, one arg){
 	return (one){.one_p = one_first(p)};
 }
 
-static one one_string_assign(one *self, one arg){
-	int len = strlen(arg.char_p);
+static one one_string_respace(one *self, one arg){
 	int unit = one_find_by_name(self, "@space_unit")->c_int;
 	
-	int new_space = (len / unit + 1) * 1024;
+	int new_space = (arg.c_int / unit + 1) * unit;
+
+	one *attr_space = one_find_by_name(self, "@space");
+	one *attr_str = one_find_by_name(self, "@str");
+
+	if(new_space > attr_space->c_int){
+		if(attr_space->c_int > 0) free(attr_str->char_p);
+		attr_str->char_p = malloc(new_space);
+		attr_space->c_int = new_space;
+	}
+
+	return *self;
+}
+
+static one one_string_assign(one *self, one arg){
+	char *str = one_call(self, "str", arg).char_p;
+
+	int len = strlen(str);
+	int unit = one_find_by_name(self, "@space_unit")->c_int;
+	
+	int new_space = (len / unit + 1) * unit;
 
 	one *attr_space = one_find_by_name(self, "@space");
 	one *attr_str = one_find_by_name(self, "@str");
@@ -39,12 +60,12 @@ static one one_string_assign(one *self, one arg){
 		attr_space->c_int = new_space;
 	}
 	
-	strcpy(attr_str->char_p, arg.char_p);
+	strcpy(attr_str->char_p, str);
 
 	return *self;
 }
 
-static one one_string_equal(one *self, one arg){
+static one one_string_cmp(one *self, one arg){
 	one *p = one_find_by_name(&arg, "@str");
 	
 	char *str = (p == NULL ? NULL : p->char_p);
@@ -58,6 +79,24 @@ static one one_string_equal(one *self, one arg){
 
 static one one_string_append(one *self, one arg){
 	return *self;
+}
+
+static one one_string_name(one *self, one arg){
+	return (one){.char_p = one_atom("string")};
+}
+
+static one one_string_str(one *self, one arg){
+	char *str;
+
+	one *p = one_find(&arg, "name", "method");
+	if(p != NULL){
+		one_assert(p->method(self, one_empty()).char_p == one_atom("string"));
+		str = one_find_by_name(&arg, "@str")->char_p;
+	}else{
+		str = arg.char_p;
+	}
+
+	return (one){.char_p = str};
 }
 
 one *one_string(void){
@@ -83,7 +122,7 @@ one *one_string(void){
 
 	p->name = one_atom("==");
 	p->type = one_atom("method");
-	p->method = one_string_equal;
+	p->method = one_string_cmp;
 
 	one_join(p, one_new(1));
 	p = one_last(p);
@@ -91,6 +130,27 @@ one *one_string(void){
 	p->name = one_atom("@space_unit");
 	p->type = one_atom("int");
 	p->c_int = 1024;
+	
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("name");
+	p->type = one_atom("method");
+	p->method = one_string_name;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("str");
+	p->type = one_atom("method");
+	p->method = one_string_str;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("respace");
+	p->type = one_atom("method");
+	p->method = one_string_respace;
 
 	string = one_first(p);
 	return string;
--- test/test_one_string.c
+++ test/test_one_string.c
@@ -11,6 +11,9 @@ void test_one_string(void){
 	one_assert(one_find(string, "=", "method") != NULL);
 	one_assert(one_find(string, "==", "method")!= NULL);
 	one_assert(one_find(string, "@space_unit", "int") != NULL);
+	one_assert(one_find(string, "name", "method")!= NULL);
+	one_assert(one_find(string, "str", "method")!= NULL);
+	one_assert(one_find(string, "respace", "method")!= NULL);
 }
 
 void test_one_string_new(void){
@@ -29,6 +32,20 @@ void test_one_string_new(void){
 	one_assert(one_parent(p) == one_string());
 }
 
+void test_one_string_respace(void){
+	printf("test_one_string_respace\n");
+	
+	one *p = one_call(one_string(), "new", one_empty()).one_p;
+	one_call(p, "respace", (one){.c_int = 100});
+	one_assert(one_find_by_name(p, "@space")->c_int == 1024); 
+
+	one_call(p, "respace", (one){.c_int = 1000});
+	one_assert(one_find_by_name(p, "@space")->c_int == 1024); 
+	
+	one_call(p, "respace", (one){.c_int = 1024});
+	one_assert(one_find_by_name(p, "@space")->c_int == 2048); 
+}
+
 void test_one_string_assign(void){
 	printf("test_one_string_assign\n");
 	
@@ -41,11 +58,11 @@ void test_one_string_assign(void){
 	one *p2 = one_call(one_string(), "new", one_empty()).one_p;
 	one_call(p2, "=", *p);
 
-	one_assert(one_call(p, "==", *p2).c_int == 0);
+	//one_assert(one_call(p, "==", *p2).c_int == 0);
 }
 
-void test_one_string_equal(void){
-	printf("test_one_string_equal\n");
+void test_one_string_cmp(void){
+	printf("test_one_string_cmp\n");
 
 	one *p = one_call(one_string(), "new", one_empty()).one_p;
 	one_call(p, "=", (one){.char_p = "abc"});
@@ -56,13 +73,36 @@ void test_one_string_equal(void){
 	one_assert(one_call(p, "==", *p).c_int == 0);
 }
 
+void test_one_string_name(void){
+	printf("test_one_string_name\n");
+
+	one *string = one_string();
+	one_assert(one_call(string, "name", one_empty()).char_p == one_atom("string"));
+
+	one *s = one_call(string, "new", one_empty()).one_p;
+	one_assert(one_call(s, "name", one_empty()).char_p == one_atom("string"));
+}
+
+void test_one_string_str(void){
+	printf("test_one_string_str\n");
+	
+	one *str = one_call(one_string(), "new", one_empty()).one_p;
+	one_assert(one_call(str, "str", *str).char_p == one_find_by_name(str, "@str")->char_p);
+
+	one str2 = {.char_p = "abc"};
+	one_assert(one_call(str, "str", str2).char_p == str2.char_p);
+}
+
 int main(void){
 	printf("start testing...\n\n");
 
 	one_test_success(test_one_string);
 	one_test_success(test_one_string_new);
+	one_test_success(test_one_string_cmp);
+	one_test_success(test_one_string_name);
+	one_test_success(test_one_string_respace);
 	one_test_success(test_one_string_assign);
-	one_test_success(test_one_string_equal);
+	one_test_success(test_one_string_str);
 
 	printf("test end\n");
 
