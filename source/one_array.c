a91e89f7aa0039b80d848dcf7a9955e05f7ce181
mark <bjmark@sina.com>
2021-03-03 16:57:33 +0800

    one_array_respace

--- source/one_array.c
+++ source/one_array.c
@@ -1,7 +1,65 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "one.h"
 
 static one one_array_new(one *self, one arg){
+	one *p = one_new(1);
+
+	p->name = one_atom("@col");
+	p->type = one_atom("int");
+	p->c_int = one_find_by_name(self, "@col")->c_int;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("@row");
+	p->type = one_atom("int");
+	p->c_int = 0;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("@len");
+	p->type = one_atom("int");
+	p->c_int = 0;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("@first_row");
+	p->type = one_atom("void_p");
+	p->void_p = NULL;
+
+	one_attach(p, self);
+
+	return (one){.one_p = one_first(p)};
+}
+
+one one_array_respace(one *self, one arg){
+	one *col = one_find_by_name(self, "@col");
+	one *row = one_find_by_name(self, "@row");
+	one *first_row = one_find_by_name(self, "@first_row");
+
+	int space = row->c_int * col->c_int;
+	if(space >= arg.c_int) return *self;
+
+	int row2 = arg.c_int / col->c_int;
+	if(arg.c_int % col->c_int != 0 ) row2++;
+
+	one ***p = malloc(row2 * sizeof(***p));
+
+	int i;
+	for(i = 0; i < row->c_int; i++){
+		p[i] = (one **)first_row->void_p + i;
+	}
+
+	for(; i < row2; i++){
+		p[i] = malloc(col->c_int * sizeof(one **));
+	}
+
+	first_row->void_p = p;
+	row->c_int = row2;
+
 	return *self;
 }
 
@@ -15,6 +73,20 @@ one *one_array(void){
 	p->name = one_atom("new");
 	p->type = one_atom("method");
 	p->method = one_array_new; 
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("@col");
+	p->type = one_atom("int");
+	p->c_int = 128;
+
+	one_join(p, one_new(1));
+	p = one_last(p);
+
+	p->name = one_atom("respace");
+	p->type = one_atom("method");
+	p->method = one_array_respace; 
 
 	array = one_first(p);
 	return array;
--- test/test_one_array.c
+++ test/test_one_array.c
@@ -8,12 +8,50 @@ void test_one_array(void){
 	one *p = one_array();
 	
 	one_assert(one_find(p, "new", "method") != NULL);
+	one_assert(one_find(p, "@col", "int") != NULL);
+	one_assert(one_find(p, "respace", "method") != NULL);
+}
+
+void test_one_array_new(void){
+	printf("test_one_array_new\n");
+
+	one *p = one_call(one_array(), "new", one_empty()).one_p;
+
+	one_assert(one_find_by_name(p, "@col")->c_int == one_find_by_name(one_array(), "@col")->c_int);
+	one_assert(one_find_by_name(p, "@row")->c_int == 0);
+	one_assert(one_find_by_name(p, "@len")->c_int == 0);
+	one_assert(one_find_by_name(p, "@first_row")->void_p == NULL);
+}
+
+void test_one_array_respace(void){
+	printf("test_one_array_respace\n");
+
+	one *p = one_call(one_array(), "new", one_empty()).one_p;
+
+	one_call(p, "respace", (one){.c_int = 1});
+	one_assert(one_find_by_name(p, "@row")->c_int == 1);
+
+	one_call(p, "respace", (one){.c_int = 100});
+	one_assert(one_find_by_name(p, "@row")->c_int == 1);
+	
+	one_call(p, "respace", (one){.c_int = 128});
+	one_assert(one_find_by_name(p, "@row")->c_int == 1);
+	
+	one_call(p, "respace", (one){.c_int = 128 + 1});
+	one_assert(one_find_by_name(p, "@row")->c_int == 2);
+
+	one *p2 = one_find_by_name(p, "@first_row");
+	one ***p3 = p2->void_p;
+	p3[0][0] = p2;
+	p3[1][1] = p2;
 }
 
 int main(void){
 	printf("start testing...\n\n");
 	
 	one_test_success(test_one_array);
+	one_test_success(test_one_array_new);
+	one_test_success(test_one_array_respace);
 
 	printf("test end\n");
 	return 1;
